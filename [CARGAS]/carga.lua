0c2dd5e7842b4d5b6ce5c71edd9bce040c10ffb3
temporaltime93 <temporaltime93@gmail.com>
2025-07-05 18:16:13 -0500

    💾 Edición desde el editor web

--- [CARGAS]/carga.lua
+++ [CARGAS]/carga.lua
@@ -84,7 +84,7 @@ end)
 
 -- * Progresión de barra con tiempo
 task.spawn(function()
-    local duracion = 7
+    local duracion = 9
     local pasos = 100
     for i = 1, pasos do
         barraProgreso.Size = UDim2.new(i / pasos, 0, 1, 0)
