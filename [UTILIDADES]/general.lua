af23a018eb296d52e3701846b86852e633f6f3ab
temporaltime93 <temporaltime93@gmail.com>
2025-07-06 08:13:30 -0500

    💾 Edición desde el editor web

--- [UTILIDADES]/general.lua
+++ [UTILIDADES]/general.lua
@@ -1,21 +1,21 @@
 local baseURL = "https://raw.githubusercontent.com/temporaltime93/-PROTOTYPE-/refs/heads/main/%5BUTILIDADES%5D/"
 
 local scripts = {
-  tablero = "caja.lua",
-  mensajes = "mensajes.lua",
+	tablero = "caja.lua",
+	mensajes = "mensajes.lua",
 }
 
 for nombre, activo in pairs(_G.selecciones or {}) do
-  if activo and scripts[nombre] then
-    local url = baseURL .. scripts[nombre]
-    local success, result = pcall(function()
-      return loadstring(game:HttpGet(url))()
-    end)
+	if activo and scripts[nombre] then
+		local url = baseURL .. scripts[nombre]
+		local success, result = pcall(function()
+			return loadstring(game:HttpGet(url))()
+		end)
 
-    if success then
-      print("✅ Módulo cargado:", nombre)
-    else
-      warn("❌ Error al cargar:", nombre, result)
-    end
-  end
+		if success then
+			print("✅ Módulo cargado:", nombre)
+		else
+			warn("❌ Error al cargar:", nombre, result)
+		end
+	end
 end
