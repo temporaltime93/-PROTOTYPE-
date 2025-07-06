1251a7594afb945c788f463c95cfe79d3bc7fbb9
temporaltime93 <temporaltime93@gmail.com>
2025-07-06 07:47:36 -0500

    🆕 Crear archivo: [MODULOS]/mensajes.lua

---
+++ [MODULOS]/mensajes.lua
@@ -0,0 +1,13 @@
+local function log(modo, mensaje)
+	if modo == "info" then
+		print("✅ [INFO] " .. mensaje)
+	elseif modo == "warn" then
+		warn("⚠️ [ADVERTENCIA] " .. mensaje)
+	elseif modo == "error" then
+		error("❌ [ERROR] " .. mensaje)
+	elseif modo == "help" then
+		print("🔷 [AYUDA] " .. mensaje)
+	else
+		print("🔘 [LOG] " .. mensaje)
+	end
+end
