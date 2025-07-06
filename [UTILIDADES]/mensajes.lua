44e7ec3faf2c45a8ff54ef2285d782a089f5c39c
temporaltime93 <temporaltime93@gmail.com>
2025-07-06 08:12:32 -0500

    💾 Edición desde el editor web

--- [UTILIDADES]/mensajes.lua
+++ [UTILIDADES]/mensajes.lua
@@ -1,20 +1,18 @@
+-- mensajes.lua
 local M = {}
 
 function M.log(tipo, mensaje)
-	if tipo == "info" then
-		print("✅ [INFO] " .. mensaje)
-	elseif tipo == "warn" then
-		warn("⚠️ [ADVERTENCIA] " .. mensaje)
-	elseif tipo == "error" then
-		error("❌ [ERROR] " .. mensaje)
-	elseif tipo == "help" then
-		print("🔷 [AYUDA] " .. mensaje)
-	else
-		print("🔘 [LOG] " .. mensaje)
-	end
+	-- (tu código log)
 end
 
--- ? Registrar global para uso inmediato
-_G.log = M.log
+-- * Simular que "está escuchando" o que corre algo en segundo plano
+spawn(function()
+	while true do
+		wait(5)
+		M.log("info", "⌛ Sistema en escucha activa...")
+	end
+end)
 
+-- Registrar global
+_G.log = M.log
 return M
